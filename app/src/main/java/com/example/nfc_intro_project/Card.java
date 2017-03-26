70fc4ba71035be3679c94b617da6a05242373e8c
John <johnlarmie@gmail.com>
2017-03-26 09:41:31 -0400

    adding missing commit files

---
+++ app/src/main/java/com/example/nfc_intro_project/Card.java
@@ -0,0 +1,157 @@
+package com.example.nfc_intro_project;
+
+import android.content.Context;
+
+
+/**
+ * Created by edmondcotterell on 2016-04-21.
+ */
+public class Card {
+
+    private String name;
+    private String company;
+    private String email;
+    private String number;
+
+    public Card(String name, String company, String email, String number) {
+        this.name = name;
+        this.company = company;
+        this.email = email;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    //    private int listID;
+//    private String listName;
+//    private String store;
+//
+//    public Card(int listID, String listName, String store)
+//    {
+//        this.listID = listID;
+//        this.listName = listName;
+//        this.store = store;
+//    }
+//
+////    public int getTotalBoughtItems(Context context) {
+//
+//        ShoppingListItemController itemController =  new ShoppingListItemController(context,listID);
+//        int totalBought = 0;
+//        for(ShoppingListItem item: itemController.getShoppingListItems())
+//        {
+//            if(item.getItemBought() == 1)
+//                totalBought++;
+//        }
+//
+//        return totalBought;
+//    }
+//
+//    public double getTotalPriceUnchecked(Context context)
+//    {
+//        ShoppingListItemController itemController =  new ShoppingListItemController(context,listID);
+//        double uncheckedPrice = 0.00;
+//
+//        for(ShoppingListItem item: itemController.getShoppingListItems())
+//        {
+//            if(item.getItemBought() == 0)
+//                uncheckedPrice+=(item.getItemPrice()*item.getItemQuantity());
+//        }
+//
+//        return uncheckedPrice;
+//    }
+//
+//    public int getListID() {
+//        return listID;
+//    }
+//
+//    public void setListID(int listID) {
+//        this.listID = listID;
+//    }
+//
+//    public String getListName() {
+//        return listName;
+//    }
+//
+//    public void setListName(String listName) {
+//        this.listName = listName;
+//    }
+//
+//    /*
+//    * Right now, we store the index of where the store name is located in
+//    * @see in Settings.getStoreOptions(Context context)
+//    * N/B: Actual store name is not stored in db, just index
+//    *
+//    * */
+//    public String getStore() {
+//        return store;
+//    }
+//
+//    public void setStore(String store) {
+//        this.store = store;
+//    }
+//
+//    public String getFormattedShoppingListString(Context context)
+//    {
+//        ShoppingListItemController itemController =  new ShoppingListItemController(context,listID);
+//        String formattedList = "";
+//
+//        formattedList+="Shopping List\n";
+//        formattedList+="_______________\n\n";
+//
+//        for(ShoppingListItem item: itemController.getShoppingListItems())
+//        {
+//            String price = "";
+//            String totalPrice = "- -";
+//
+//            if(item.getItemPrice() > 0)
+//            {
+//                price = context.getResources().getString(R.string.dollar_sign) + String.valueOf(item.getItemPrice());
+//                totalPrice = context.getResources().getString(R.string.dollar_sign) + Util.roundToDecimals(item.getItemPrice()*item.getItemQuantity());
+//
+//            }
+//            formattedList+=String.format("%s(x%s) %s    =   \t%3s\n\n", item.getItemName(), String.valueOf(item.getItemQuantity()), price, totalPrice);
+//
+//        }
+//
+//        formattedList+="\n*** SHOPIZY APP ***";
+//
+//        return formattedList;
+//    }
+//
+//    public int getListSize(Context context)
+//    {
+//        return new ShoppingListItemController(context,listID).getShoppingListItems().size();
+//    }
+
+
+}
---
+++ app/src/main/java/com/example/nfc_intro_project/CardListAdapter.java
@@ -0,0 +1,199 @@
+package com.example.nfc_intro_project;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.SparseBooleanArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by edmondcotterell on 2016-04-21.
+ */
+
+public class CardListAdapter extends SelectableAdapter<CardListAdapter.ViewHolder> {
+    private ArrayList<Card> mDataset;
+    private final Context mContext;
+    public SparseBooleanArray selectedItems;
+    private ViewHolder.ClickListener clickListener;
+    private OnCompleteListener mListener;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener,
+            View.OnLongClickListener {
+
+        // each data item is just a string in this case
+        private CardView cardView;
+        private TextView companyNameView;
+        private TextView phoneNumberView;
+        private TextView companyEmailView;
+        private RelativeLayout cardLayout;
+        private ViewHolder.ClickListener listener;
+
+        public ViewHolder(CardView v, ClickListener listener) {
+            super(v);
+            cardView = v;
+            companyNameView = (TextView) v.findViewById(R.id.Company_name);
+            phoneNumberView = (TextView) v.findViewById(R.id.Phone_number);
+            companyEmailView = (TextView) v.findViewById(R.id.Company_email);
+            cardLayout = (RelativeLayout)v.findViewById(R.id.card_layout_sl);
+            this.listener = listener;
+
+            v.setOnClickListener(this);
+            v.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (listener != null) {
+                listener.onItemClicked(getPosition());
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+
+            if (listener != null) {
+                return listener.onItemLongClicked(getPosition());
+            }
+            return false;
+        }
+
+        public interface ClickListener {
+            public void onItemClicked(int position);
+            public boolean onItemLongClicked(int position);
+        }
+    }
+
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public CardListAdapter(ArrayList<Card> myDataset, Context mContext, ViewHolder.ClickListener clickListener) {
+        mDataset = myDataset;
+        this.mContext = mContext;
+        selectedItems = new SparseBooleanArray();
+        this.clickListener = clickListener;
+
+        //implement oncomplete listener to let main activity know when list empty
+        try {
+            this.mListener = (OnCompleteListener)mContext;
+        }
+        catch (final ClassCastException e) {
+            throw new ClassCastException(mContext.toString() + " must implement OnCompleteListener");
+        }
+    }
+
+    // Create new views (invoked by the cardView manager)
+    @Override
+    public CardListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                         int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_business_card, parent, false);
+        ViewHolder vh = new ViewHolder((CardView) v, clickListener);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the cardView manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, final int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+
+        Card listItem = mDataset.get(position);
+
+        //set list name
+        TextView listName = (TextView) holder.cardView.findViewById(R.id.Contact_name);
+        listName.setText(listItem.getName().toUpperCase());
+
+        TextView listeEmail= (TextView) holder.cardView.findViewById(R.id.Company_email);
+        listeEmail.setText(listItem.getEmail().toUpperCase());
+
+        TextView listNumber = (TextView) holder.cardView.findViewById(R.id.Phone_number);
+        listNumber.setText(listItem.getNumber().toUpperCase());
+
+        TextView listCompanyName = (TextView) holder.cardView.findViewById(R.id.Company_name);
+        listCompanyName.setText(listItem.getCompany().toUpperCase());
+
+        // Highlight the item if it's selected
+        if(isSelected(position))
+            holder.cardLayout.setBackgroundColor(mContext.getResources().getColor(R.color.select));
+        else
+            holder.cardLayout.setBackgroundColor(mContext.getResources().getColor(R.color.unselect));
+
+    }
+
+    // Return the size of your dataset (invoked by the cardView manager)
+    @Override
+    public int getItemCount() {
+
+        //tell main activity to do something each
+        // time the list count is updated
+        mListener.onComplete(mDataset.size());
+        return mDataset.size();
+    }
+
+    public void removeItem(int position) {
+        mDataset.remove(position);
+        notifyItemRemoved(position);
+    }
+
+    public int getDataSetSize()
+    {
+        return mDataset.size();
+    }
+
+    //just to let the main activity noe when the list is empty
+    public static interface OnCompleteListener {
+        public abstract void onComplete(int dataSetSize);
+    }
+
+    public void removeItems(List<Integer> positions) {
+        // Reverse-sort the list
+        Collections.sort(positions, new Comparator<Integer>() {
+            @Override
+            public int compare(Integer lhs, Integer rhs) {
+                return rhs - lhs;
+            }
+        });
+
+        // Split the list in ranges
+        while (!positions.isEmpty()) {
+            if (positions.size() == 1) {
+                removeItem(positions.get(0));
+                positions.remove(0);
+            } else {
+                int count = 1;
+                while (positions.size() > count && positions.get(count).equals(positions.get(count - 1) - 1)) {
+                    ++count;
+                }
+
+                if (count == 1) {
+                    removeItem(positions.get(0));
+                } else {
+                    removeRange(positions.get(count - 1), count);
+                }
+
+                for (int i = 0; i < count; ++i) {
+                    positions.remove(0);
+                }
+            }
+        }
+    }
+
+    private void removeRange(int positionStart, int itemCount) {
+        for (int i = 0; i < itemCount; ++i) {
+            mDataset.remove(positionStart);
+        }
+        notifyItemRangeRemoved(positionStart, itemCount);
+    }
+}
